aa1851964eb77a66e89341c761e6c823c59ab3d9
ItzSave <itzsaveyt@gmail.com>
2021-12-01 19:35:05 -0600

    0.4 - Add & Remove Roles + Better Messages - Made the alerts role add and remove on button click. - Added event.deferReply() to stop errors. - Changed up the embed message.

--- src/main/java/SaveBot.java
+++ src/main/java/SaveBot.java
@@ -7,10 +7,15 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
+
 import javax.security.auth.login.LoginException;
 
 public class SaveBot {
+
+
     public static void main(String[] args) throws LoginException, InterruptedException {
+
+
         JDA jda = JDABuilder.createDefault("NTQ1NjUwNjA1NTc0NTg2MzY4.XGWeug.082qEmtDbPK1HuHDoKhVdBPqkTI")
                 .addEventListeners(new PingCommand())
                 .addEventListeners(new EmbedCommand())
--- src/main/java/commands/ButtonCommand.java
+++ src/main/java/commands/ButtonCommand.java
@@ -2,10 +2,7 @@ package commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -20,7 +17,7 @@ public class ButtonCommand extends ListenerAdapter {
 
         if (event.getMessage().getContentRaw().equals("!createrolereact")) {
 
-            if (!event.getAuthor().getName().equals("Test")){
+            if (!event.getAuthor().getId().equals("111984776272949248")) {
                 System.out.println("A user tried to run this command that does not have permission!");
                 EmbedBuilder eb = new EmbedBuilder();
                 eb.setColor(0xFF0000);
@@ -37,13 +34,16 @@ public class ButtonCommand extends ListenerAdapter {
                 return;
             }
 
-            System.out.println("Hello command has been run.");
+            System.out.println("Role React Message has been added.");
+
 
             MessageChannel channel = event.getChannel();
 
             EmbedBuilder eb = new EmbedBuilder();
-            eb.setColor(0xFF00FF);
-            eb.setTitle("This is a title");
+            eb.setColor(0x1279ff);
+            eb.setTitle("Alert Notifications");
+            eb.setDescription("Click on the buttons below to add or remove roles.");
+            eb.setFooter(event.getJDA().getSelfUser().getName());
             eb.setTimestamp(Instant.now());
 
             MessageBuilder message = new MessageBuilder();
@@ -52,7 +52,7 @@ public class ButtonCommand extends ListenerAdapter {
 
             TextChannel textChannel = event.getGuild().getTextChannelsByName("ideas", true).get(0);
             textChannel.sendMessage(message.build())
-                    .setActionRow((Button.primary("hello", "Click Me"))).queue(); // Button with only a label
+                    .setActionRow((Button.success("Alerts","Alerts"))).queue(); // Button with only a label
 
         } else {
             return;
@@ -60,14 +60,23 @@ public class ButtonCommand extends ListenerAdapter {
     }
 
     public void onButtonClick(ButtonClickEvent event) {
-        if (event.getComponentId().equals("hello")) {
-            event.reply("Hello :)").queue();
+        //Checking for when a user clicks on the alerts button.
+        if (event.getComponentId().equals("Alerts")) {
+
             Role role = event.getGuild().getRolesByName("Notifications", false).get(0);
 
+            event.deferReply().queue();
 
-            event.getGuild().addRoleToMember(event.getMember(), role).queue();
-
-
+            if (event.getMember().getRoles().contains(role)) {
+                System.out.println("User " + event.getUser().getName() + " already has the notification role removing it...");
+                event.getGuild().removeRoleFromMember(event.getMember(), role).queue();
+            } else {
+                System.out.println("Adding notification role to user...");
+                event.getGuild().addRoleToMember(event.getMember(), role).queue();
+            }
+            event.getHook().deleteOriginal().queue();
         }
+
+        //Checking for when a user clicks on the giveaway button.
     }
 }
